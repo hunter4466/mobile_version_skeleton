79c2c3c2513aaf614da14653f7cf9b42ee24064b
Mario Chois <mario.chois.navarro@gmail.com>
2021-06-21 23:57:09 -0500

    Working with dom

--- Assets/Javascript/index.js
+++ Assets/Javascript/index.js
@@ -2,38 +2,37 @@ const menu_btn = document.getElementById('menu_btn');
 const display_menu = document.getElementById('display_menu');
 const close_menu_btn = document.getElementById('close_menu_btn');
 
-let sec = []
+let sec = [];
 for (let i = 1; i < 6;i += 1) {
     sec.push(document.getElementById(`sec_${i}`));
 }
 
-let links = []
+let links = [];
 for (let i = 1; i < 4; i += 1) {
-  links.push(document.getElementById(`menu_link_${i}`))
+  links.push(document.getElementById(`menu_link_${i}`));
 }
 
 menu_btn.addEventListener('click',(event)=>{
-  console.log('button works')
   event.preventDefault();
-  display_menu.style = "display: flex; animation-name: menu_display_anim;"
+  display_menu.style = 'display: flex; animation-name: menu_display_anim;';
   for (let i = 0; i < sec.length; i += 1) {
-    sec[i].style = 'filter: blur(4px); -webkit-filter: blur(4px);'
+    sec[i].style = 'filter: blur(4px); -webkit-filter: blur(4px);';
   }
 })
 
 close_menu_btn.addEventListener('click',(Event)=>{
   Event.preventDefault();
-  display_menu.style = "display: flex; animation-name: menu_close_anim;"
+  display_menu.style = 'display: flex; animation-name: menu_close_anim;';
   for (let i = 0; i < sec.length; i += 1){
     sec[i].style = ''
   }
-  setTimeout(()=>{display_menu.style = "display: none;"},450)
+  setTimeout(()=>{display_menu.style = 'display: none;'},450)
 })
 
 for(let i = 0; i < links.length; i += 1){
   links[i].addEventListener('click',(Event)=>{
     Event.preventDefault();
-    display_menu.style = "display: flex; animation-name: menu_close_anim;"
+    display_menu.style = 'display: flex; animation-name: menu_close_anim;';
     for (let i = 0; i < sec.length; i += 1) {
       sec[i].style = '';
     }
