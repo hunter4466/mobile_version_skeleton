35d5cdffd31a2dd0f69ee549115ccc5f855b1220
Mario Chois <mario.chois.navarro@gmail.com>
2021-06-23 00:28:27 -0500

    Added dinamic cards on javascrip

--- Assets/Javascript/index2.js
+++ Assets/Javascript/index2.js
@@ -1,41 +1,50 @@
-const workInformation = [{imagePath:'./Assets/Images/portfolio_pictures/sn1.png',
-                                    title:'Tonic',
-                                    features:['CANOPY', 'Back End Dev', '2015'],
-                                    description:'A daily selection of privately perzonalized reads; no account or sign-ups required.',
-                                    languages:['html','css','javascript'],
-                                    buttonText:'See Project',
-                                    },
-                                    {imagePath:'./Assets/Images/portfolio_pictures/sn2.png',
-                                    title:'Multi-Post Stories',
-                                    features:['CANOPY', 'Back End Dev', '2015'],
-                                    description:'A daily selection of privately perzonalized reads; no account or sign-ups required.',
-                                    languages:['html','css','javascript'],
-                                    buttonText:'See Project',
-                                    },
-                                    {imagePath:'./Assets/Images/portfolio_pictures/sn3.png',
-                                    title:'Tonic',
-                                    features:['CANOPY', 'Back End Dev', '2015'],
-                                    description:'A daily selection of privately perzonalized reads; no account or sign-ups required.',
-                                    languages:['html','css','javascript'],
-                                    buttonText:'See Project',
-                                    },
-                                    {imagePath:'./Assets/Images/portfolio_pictures/sn4.png',
-                                    title:'Multi-Post Stories',
-                                    features:['CANOPY', 'Back End Dev', '2015'],
-                                    description:'A daily selection of privately perzonalized reads; no account or sign-ups required.',
-                                    languages:['html','css','javascript'],
-                                    buttonText:'See Project',
-                                    },
-                                    ]
 
 const workSection = document.getElementById('sec_3')
 const main_ul = document.createElement('ul')
+const workDetail = document.getElementById('detail_sec')
+const sectObj = [];
+        for (let i = 1; i < 6; i += 1) {
+        sectObj.push(document.getElementById(`sec_${i}`));
+        }
+
+const workInformation = [{imagePath:'./Assets/Images/portfolio_pictures/sn1.png',
+        title:'Tonic',
+        features:['CANOPY', 'Back End Dev', '2015'],
+        description:'A daily selection of privately perzonalized reads; no account or sign-ups required.',
+        languages:['html','css','javascript'],
+        buttonText:'See Project',
+        detailText:" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ",
+        },
+        {imagePath:'./Assets/Images/portfolio_pictures/sn2.png',
+        title:'Multi-Post Stories',
+        features:['CANOPY', 'Back End Dev', '2015'],
+        description:'A daily selection of privately perzonalized reads; no account or sign-ups required.',
+        languages:['html','css','javascript'],
+        buttonText:'See Project',
+        detailText:" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ",
+        },
+        {imagePath:'./Assets/Images/portfolio_pictures/sn3.png',
+        title:'Tonic',
+        features:['CANOPY', 'Back End Dev', '2015'],
+        description:'A daily selection of privately perzonalized reads; no account or sign-ups required.',
+        languages:['html','css','javascript'],
+        buttonText:'See Project',
+        detailText:" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ",
+        },
+        {imagePath:'./Assets/Images/portfolio_pictures/sn4.png',
+        title:'Multi-Post Stories',
+        features:['CANOPY', 'Back End Dev', '2015'],
+        description:'A daily selection of privately perzonalized reads; no account or sign-ups required.',
+        languages:['html','css','javascript'],
+        buttonText:'See Project',
+        detailText:" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ",
+        },
+        ]
+
+
 workSection.appendChild(main_ul)
 
-
-
 function create(obj) { return document.createElement(obj)}
-
 function constructor(obj) { 
     for (let i = 0; i < obj.length; i += 1){
         if(obj[i].length == 2){
@@ -55,8 +64,32 @@ function constructor(obj) {
                 obj[i][1].className = obj[i][2]
                 obj[i][1].innerHTML = obj[i][3]
              }
-            
-         }
+        }
+         else if(obj[i].length == 5){
+            if(obj[i][2]==null && obj[i][3]==null){
+               obj[i][0].appendChild(obj[i][1])
+               obj[i][1].id = obj[i][4]
+            }
+            else if(obj[i][2]==null){
+               obj[i][0].appendChild(obj[i][1])
+               obj[i][1].innerHTML = obj[i][3]
+               obj[i][1].id = obj[i][4]
+            }
+            else if(obj[i][3]==null){
+                obj[i][0].appendChild(obj[i][1])
+                obj[i][1].className = obj[i][2]
+                obj[i][1].id = obj[i][4]
+             }
+             else{
+                obj[i][0].appendChild(obj[i][1])
+                obj[i][1].innerHTML = obj[i][3]
+                obj[i][1].className = obj[i][2]
+                obj[i][1].id = obj[i][4]
+
+             }
+           
+        }
+         
 }
 }
 
@@ -94,7 +127,8 @@ for (let i = 0; i < workInformation.length; i += 1) {
    let ElementsArray = [
     [main_ul,workBox],
     [workBox,imageBox,'preview sub_box'],
-    [workBox,contentBox,'sub_box_2'],[contentBox,titleBox,'title item'],
+    [workBox,contentBox,'sub_box_2'],
+    [contentBox,titleBox,'title item'],
     [titleBox,titleH1,null,workInformation[i].title],
     [contentBox,featuresBox,'features item'],
     [featuresBox,feature1Li],
@@ -120,8 +154,8 @@ for (let i = 0; i < workInformation.length; i += 1) {
     [buttonDiv,buttonDivH3],
     [buttonDivH3,buttonDivH3a,null,workInformation[i].buttonText]
    ]
-
    constructor(ElementsArray)
+
     if(i %2 == 0){
         workBox.className = 'box';
     }
@@ -131,4 +165,92 @@ for (let i = 0; i < workInformation.length; i += 1) {
     imageBox.src = workInformation[i].imagePath
     imageBox.alt = "preview"
 
+    buttonDiv.addEventListener('click',(event)=>{
+        let detailPreBackground = create('div')
+        let detailBackground = create('div')
+        let det_close_btn = create('img')
+        let detTitle = create('h1')
+        let detFeatures = create('div')
+        let detFeat1 = create('h1')
+        let detdot1 = create('img')
+        let detFeat2 = create('h1')
+        let detdot2 = create('img')
+        let detFeat3 = create('h1')
+        let detImage = create('img')
+        let detDescription = create('p')
+        let languagesDiv = create('div')
+        let languagesDivLang1 = create('h6')
+        let languagesDivLang2 = create('h6')
+        let languagesDivLang3 = create('h6')
+        let buttonsDiv = create('div')
+        let buttonsDivBtn1 = create('div')
+        let button1Content1 = create('h3')
+        let button1Content2 = create('img')
+        let buttonsDivBtn2 = create('div')
+        let button2Content1 = create('h3')
+        let button2Content2 = create('img')
+
+        detImage.src = workInformation[i].imagePath
+        detImage.alt = "preview"
+        elmArray = [
+            [workDetail,detailPreBackground,'detail_pre_background'],
+            [workDetail,detailBackground,'detail_background'],
+            [detailBackground,det_close_btn,'det_close_btn'],
+            [detailBackground,detTitle,'detail_title',workInformation[i].title],
+            [detailBackground,detFeatures,'detail_features'],
+            [detFeatures,detFeat1,'det_feature det_f_1',workInformation[i].features[0]],
+            [detFeatures,detdot1,'det_feature_dot'],
+            [detFeatures,detFeat2,'det_feature det_f_1',workInformation[i].features[1]],
+            [detFeatures,detdot2,'det_feature_dot'],
+            [detFeatures,detFeat3,'det_feature det_f_1',workInformation[i].features[2]],
+            [detailBackground,detImage,'detail_img'],
+            [detailBackground,detDescription,'detail_descr',workInformation[i].detailText],
+            [detailBackground,languagesDiv,'language_box'],
+            [languagesDiv,languagesDivLang1,'language_prop',workInformation[i].languages[0]],
+            [languagesDiv,languagesDivLang2,'language_prop',workInformation[i].languages[1]],
+            [languagesDiv,languagesDivLang3,'language_prop',workInformation[i].languages[2]],
+            [detailBackground,buttonsDiv,'det_buttons_div'],
+            [buttonsDiv,buttonsDivBtn1,'det_button'],
+            [buttonsDiv,buttonsDivBtn2,'det_button'],
+            [buttonsDivBtn1,button1Content1,'det_button_text','See live'],
+            [buttonsDivBtn1,button1Content2,'det_button_icon',],
+            [buttonsDivBtn2,button2Content1,'det_button_text','See Source'],
+            [buttonsDivBtn2,button2Content2,'det_button_icon'],
+            ];
+            constructor(elmArray)
+            button1Content2.src = './Assets/Images/detail_btn1.svg';
+            button2Content2.src = './Assets/Images/detail_btn2.svg';
+            det_close_btn.src = './Assets/Images/x2.svg';
+            detdot1.src = './Assets/Images/dot.png';
+            detdot2.src = './Assets/Images/dot.png';
+            detdot1.alt = 'dot';
+            detdot2.alt = 'dot';
+            button1Content2.alt = 'See live icon';
+            button2Content2.alt = 'See source';
+            det_close_btn.alt = 'Close';
+            detailBackground.style = 'animation-name: detail_open; animation-duration: 0.5s;';
+            detailPreBackground.style = 'animation-name: background-open; animation-duration: 0.5s;';
+            
+            for (let i = 0; i < 5; i += 1) {
+            sectObj[i].style = 'filter: blur(4px); -webkit-filter: blur(4px);';
+            }
+            det_close_btn.addEventListener('click',(event) =>{
+                event.preventDefault();
+                detailBackground.style = 'animation-name: detail_close;animation-duration: 1s;';
+                detailPreBackground.style = 'animation-name: background-close; animation-duration: 1s;';
+                setTimeout(()=>{detailPreBackground.remove(); detailBackground.remove();},800) 
+                for (let i = 0; i < 5; i += 1) {
+                    sectObj[i].style = '';
+                    }
+            })
+            detailPreBackground.addEventListener('click',(event) =>{
+                event.preventDefault();
+                detailBackground.style = 'animation-name: detail_close;animation-duration: 1s;';
+                detailPreBackground.style = 'animation-name: background-close; animation-duration: 1s;';
+                setTimeout(()=>{detailPreBackground.remove(); detailBackground.remove();},800) 
+                for (let i = 0; i < 5; i += 1) {
+                    sectObj[i].style = '';
+                    }
+            })
+    })
 }
