a7236fcb8f4648d0b51d0ed847d97d5a4d575dfa
Mario Chois <mario.chois.navarro@gmail.com>
2021-06-23 23:15:11 -0500

    Added E-mail upercase validation, used javascript function to hold the form post

--- Assets/Javascript/index.js
+++ Assets/Javascript/index.js
@@ -1,6 +1,45 @@
 const MenuBtn = document.getElementById('menu_btn');
 const DisplayMenu = document.getElementById('display_menu');
 const CloseMenuBtn = document.getElementById('close_menu_btn');
+const emailInput = document.getElementById('email_input');
+const announce = document.createElement('div');
+const announceText = document.createElement('p');
+const contactForm = document.getElementById('contact_form');
+const emailRegExpr = /[A-Z]+/;
+
+function formValidate() {
+  const emailValidator = emailRegExpr.test(emailInput.value);
+  if (emailValidator) {
+    if (announce) {
+      announce.remove();
+      contactForm.appendChild(announce);
+      announce.appendChild(announceText);
+      announce.className = 'announce_bad_email';
+      announceText.className = 'announce_text';
+      announceText.innerHTML = 'Email address should not contain upercase characters!';
+      emailInput.style = 'animation: email_input_error 0.3s;';
+      setTimeout(() => { emailInput.style = ''; }, 480);
+      return false;
+    }
+    contactForm.appendChild(announce);
+    announce.appendChild(announceText);
+    announce.className = 'announce_bad_email';
+    announceText.className = 'announce_text';
+    announceText.innerHTML = 'Email address should not contain upercase characters!';
+    emailInput.style = 'animation: email_input_error 0.3s;';
+    setTimeout(() => { emailInput.style = ''; }, 480);
+    return false;
+  }
+  return true;
+}
+if (emailRegExpr < 0) {
+  formValidate();
+}
+
+emailInput.addEventListener('input', (event) => {
+  event.preventDefault();
+  announce.remove();
+});
 
 const sectionsObj = [];
 for (let i = 1; i < 6; i += 1) {
