8ad85a49ccb0ceb9d790c0f48b03f5a7af1c0f82
hunter4466 <mario.chois.navarro@gmail.com>
2021-06-25 19:32:22 -0500

    fixing part 1

--- Assets/Javascript/storage.js
+++ Assets/Javascript/storage.js
@@ -1,20 +1,17 @@
-const valuesArray = [];
-valuesArray.push([document.getElementById('name_input'), 'nameInputValue']);
-valuesArray.push([document.getElementById('email_input'), 'emailInputValue']);
-valuesArray.push([document.getElementById('contact_text_area'), 'contactTextAreaValue']);
 
-function loadValues() {
-  for (let i = 0; i < valuesArray.length; i += 1) {
-    if (localStorage.getItem(valuesArray[i][1])) {
-      valuesArray[i][0].value = localStorage.getItem(valuesArray[i][1]);
-    }
-  }
-}
-window.onload = loadValues();
+let valuesArray = []
+valuesArray.push([document.getElementById('name_input'),'nameInputValue'])
+valuesArray.push([document.getElementById('email_input'),'emailInputValue'])
+valuesArray.push([document.getElementById('contact_text_area'),'contactTextValue'])
 
-function populateStorage(a, b) {
-  localStorage.setItem(b, a.value);
+const valuesObject = {}
+
+for(let i = 0; i < valuesArray.length; i += 1){
+  valuesObject[valuesArray[i][1]] = valuesArray[i][0].value
 }
+
+
+
 
 function storageAvailable(type) {
   let storage;
@@ -23,8 +20,10 @@ function storageAvailable(type) {
     const x = '__storage_test__';
     storage.setItem(x, x);
     storage.removeItem(x);
+    console.log('got here')
     return true;
   } catch (e) {
+    console.log('got over here')
     return e instanceof DOMException && (
       e.code === 22
             || e.code === 1014
@@ -36,9 +35,23 @@ function storageAvailable(type) {
 
 for (let i = 0; i < valuesArray.length; i += 1) {
   valuesArray[i][0].addEventListener('input', (event) => {
+    console.log('eventlistener working')
     event.preventDefault();
     if (storageAvailable('localStorage')) {
-      populateStorage(valuesArray[i][0], valuesArray[i][1]);
+      localStorage.setItem('indexValues',JSON.stringify(valuesObject));
     }
   });
 }
+
+
+function loadValues() {
+  for (let i = 0; i < valuesArray.length; i += 1) {
+    if (localStorage.getItem('indexValues')) {
+      var recoveredData = JSON.parse(JSON.stringify(localStorage.getItem('indexValues')))
+      var indexValue = valuesArray[i][1]
+      console.log(recoveredData[indexValue])
+      valuesArray[i][0].value = recoveredData[indexValue]
+    }
+  }
+}
+window.onload = loadValues();
